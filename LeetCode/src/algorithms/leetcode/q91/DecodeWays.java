110aaf60f9effb7b4778ab541e66d483bd252006
Vangie Shue <vangieshue@gmail.com>
2017-11-24 12:33:26 -0500

    Started LeetCode

---
+++ LeetCode/src/algorithms/leetcode/q91/DecodeWays.java
@@ -0,0 +1,12 @@
+package algorithms.leetcode.q91;
+
+public class DecodeWays {
+	public int numDecoding(String s) {
+		if(s == null || s.isEmpty())
+			return 0;
+		
+		int ans = 0;
+		
+		return ans;
+	}
+}
