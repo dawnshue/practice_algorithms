55c03a060f601833db4421745e62f900eed7c2e8
Vangie Shue <vangieshue@gmail.com>
2018-12-27 10:33:08 -0500

    Create g_longest-increasing-subseq.java

---
+++ LeetCode/g_longest-increasing-subseq.java
@@ -0,0 +1,16 @@
+/*
+https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/
+Find the length of the longest subsequence of a given sequence such that 
+all elements of the subsequence are sorted in increasing order.
+Ex. the LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} is {10, 22, 33, 50, 60, 80}.
+*/
+
+public class Solution {
+
+// O(nlogn) solution: https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
+public static int getLIS(int[] sequence) {
+
+}
+
+// O(n^2) solution
+}
